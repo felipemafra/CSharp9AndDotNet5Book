3ee64147a97460f86194041511c564914675801a
Luiz Mafra <disys_luiz.mafra@la.logicalis.com>
2021-01-01 18:11:23 -0300

    Add FibFunctional to Chapter04

---
+++ Chapter04/FibFunctional/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace FibFunctional
+{
+    class Program
+    {
+        static int FibFunctional(int term) =>
+            term switch
+            {
+                1 => 0,
+                2 => 1,
+                _ => FibFunctional(term - 1) + FibFunctional(term - 2)
+            };
+        
+
+        static void RunFibFunctional()
+        {
+            for (int i = 1; i < 30; i++)
+            {
+                WriteLine("The {0} term of the Fibonacci sequence is {1:N0}",
+                    arg0: i,
+                    arg1: FibFunctional(term:  i)
+                );
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            RunFibFunctional();
+        }
+    }
+}
