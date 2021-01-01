22b802f9cb492af145bb956f82ec5732d53e0302
Luiz Mafra <disys_luiz.mafra@la.logicalis.com>
2021-01-01 18:04:52 -0300

    Add FibImperative to Chapter04

---
+++ Chapter04/FibImperative/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using static System.Console;
+
+namespace FibImperative
+{
+    class Program
+    {
+        static int FibImperative(int term)
+        {
+            if (term == 1)
+            {
+                return 0;
+            }
+            else if (term == 2)
+            {
+                return 1;
+                
+            }
+            else
+            {
+                return FibImperative(term - 1) + FibImperative(term - 2);
+            }
+        }
+
+        static void RunFibImperative()
+        {
+            for (int i = 1; i < 30; i++)
+            {
+                WriteLine("The {0} term of the Fibonacci sequence is {1:N0}",
+                    arg0: i,
+                    arg1: FibImperative(term:  i)
+                );
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            RunFibImperative();
+        }
+    }
+}
