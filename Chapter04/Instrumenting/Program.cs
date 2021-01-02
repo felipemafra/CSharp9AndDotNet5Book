d4a1f712dbe828508d6782718d1ee171dabf58b6
Felipe Mafra <felipe.mafra@gmail.com>
2021-01-02 17:42:10 -0300

    Update Intrumenting to use appsettings.json

--- Chapter04/Instrumenting/Program.cs
+++ Chapter04/Instrumenting/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using Microsoft.Extensions.Configuration;
 
 namespace Instrumenting
 {
@@ -17,6 +18,22 @@ namespace Instrumenting
 
             Debug.WriteLine("Debug says, I am watching!");
             Trace.WriteLine("Trace says, I am watching!");
+
+            var builder = new ConfigurationBuilder()
+              .SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+
+            IConfigurationRoot configuration = builder.Build();
+
+            var ts = new TraceSwitch(displayName: "PacktSwitch", 
+              description: "This switch is set  via a JSON config.");
+            
+            configuration.GetSection("PacktSwitch").Bind(ts);
+
+            Trace.WriteLine(ts.TraceError, "Trace error");
+            Trace.WriteLineIf(ts.TraceWarning, "Trace warning");
+            Trace.WriteLineIf(ts.TraceInfo, "Trace information");
+            Trace.WriteLineIf(ts.TraceVerbose, "Trace verbose");
         }
     }
 }
