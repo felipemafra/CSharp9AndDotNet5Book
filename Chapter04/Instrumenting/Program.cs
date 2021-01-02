afb5c0d6cd8c7503c2ba5f81009be02d12b396e8
Felipe Mafra <felipe.mafra@gmail.com>
2021-01-02 17:05:04 -0300

    Add Instrumenting to Chapter04

---
+++ Chapter04/Instrumenting/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace Instrumenting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new TextWriterTraceListener(File.CreateText("log.txt"))); 
+            //                                              ^ Write to a text file in the project folder.
+            
+            Trace.AutoFlush = true;
+            //    ^ TextWriter is buffered, so this option calls 
+            //      Flush() on all listeners after writing.
+
+            Debug.WriteLine("Debug says, I am watching!");
+            Trace.WriteLine("Trace says, I am watching!");
+        }
+    }
+}
