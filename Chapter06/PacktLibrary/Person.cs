485bc4670cfbf8460773e733dca93b237498294d
Felipe Mafra <felipe.mafra@gmail.com>
2021-02-05 23:53:08 -0300

    Add Chapter06

---
+++ Chapter06/PacktLibrary/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Packt.Shared
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public List<Person> Children = new List<Person>();
+
+        public static Person Procreate(Person p1, Person p2)
+        {
+            var baby = new Person
+            {
+                Name = $"Baby of {p1.Name} and {p2.Name}",
+                DateOfBirth = DateTime.Now
+            };
+
+            p1.Children.Add(baby);
+            p2.Children.Add(baby);
+
+            return baby;
+        }
+
+        public Person ProcreateWith(Person partner)
+        {
+            return Procreate(this, partner);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} was born on a {DateOfBirth:dddd}.";
+        }
+
+    }
+}
---
+++ Chapter06/PeopleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Packt.Shared;
+using static System.Console;
+
+namespace PeopleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var harry = new Person
+            {Name = "Harry"};
+
+            var mary = new Person
+            {Name = "Mary"};
+
+            var jill = new Person
+            {Name = "Jill"};
+
+            var baby1 = mary.ProcreateWith(harry);
+            baby1.Name = "Gary";
+
+            var baby2 = Person.Procreate(harry, jill);
+
+            WriteLine($"{harry.Name} has {harry.Children.Count}");
+            WriteLine($"{mary.Name} has {mary.Children.Count}");
+            WriteLine($"{jill.Name} has {jill.Children.Count}");
+            WriteLine(
+                format: "{0}'s first children is named \"{1}\".",
+                arg0: harry.Name,
+                arg1: harry.Children[0].Name
+            );
+        }
+    }
+}
