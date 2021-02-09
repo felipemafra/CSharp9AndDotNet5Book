b1306597ed55a250523e2ec1d4ef5f4b482c1256
Felipe Mafra <felipe.mafra@gmail.com>
2021-02-08 21:04:37 -0300

    Create a IPlayable Interface and a DvdPlayer class

---
+++ Chapter06/PacktLibrary/DvdPlayer.cs
@@ -0,0 +1,17 @@
+﻿using static System.Console;
+
+namespace Packt.Shared
+{
+    class DvdPlayer : IPlayable
+    {
+        public void Pause()
+        {
+            WriteLine("DVD player is pausing.");
+        }
+
+        public void Play()
+        {
+            WriteLine("DVD player is playing.");
+        }
+    }
+}
---
+++ Chapter06/PacktLibrary/IPlayable.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+namespace Packt.Shared
+{
+    public interface IPlayable
+    {
+        void Play();
+        void Pause();
+        void Stop() // default interface implementation
+        {
+            WriteLine("Default implementation of Stop.");
+        }
+    }
+}
