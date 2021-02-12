a638076a30a9ca6ce33d4f78bd0096329e6f770c
Felipe Mafra <felipe.mafra@gmail.com>
2021-02-12 19:42:15 -0300

    Explicit casting

--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -6,12 +6,6 @@ namespace PeopleApp
 {
     class Program
     {
-        private static void Harry_Shout(object sender, EventArgs e)
-        {
-            Person p = (Person)sender;
-            WriteLine($"{p.Name} is this angry {p.AngerLevel}.");
-        }
-
         static void Main(string[] args)
         {
             Employee aliceInEmployee = new Employee
@@ -26,6 +20,12 @@ namespace PeopleApp
             aliceInPerson.WriteToConsole();
             WriteLine(aliceInEmployee.ToString());
             WriteLine(aliceInPerson.ToString());
+
+            if (aliceInPerson is Employee)
+            {
+                WriteLine($"{nameof(aliceInPerson)} IS an Employee");
+                Employee explicitAlice = (Employee)aliceInPerson;
+            }
         }
     }
 }
