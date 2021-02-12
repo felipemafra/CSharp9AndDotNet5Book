f651a55c2ef6ed49df2fa2bfe20d965a7210f100
Felipe Mafra <felipe.mafra@gmail.com>
2021-02-12 20:29:05 -0300

    Using extensio methods to reuse funtionality

--- Chapter06/PacktLibrary/StringExtensions.cs
+++ Chapter06/PacktLibrary/StringExtensions.cs
@@ -2,9 +2,9 @@
 
 namespace Packt.Shared
 {
-    public class StringExtensions
+    public static class StringExtensions
     {
-        public static bool IsValidEmail(string input)
+        public static bool IsValidEmail(this string input)
         {
             return Regex.IsMatch(input, @"[a-zA-Z0-9\.-_]+@[a-zA-Z0-9\.-_]+");
         }
--- Chapter06/PeopleApp/Program.cs
+++ Chapter06/PeopleApp/Program.cs
@@ -13,11 +13,11 @@ namespace PeopleApp
 
             WriteLine("{0} is a valid e-mail address: {1}",
                 arg0: email1,
-                arg1: StringExtensions.IsValidEmail(email1));
+                arg1: email1.IsValidEmail());
 
             WriteLine("{0} is a valid e-mail address: {1}",
                 arg0: email2,
-                arg1: StringExtensions.IsValidEmail(email2));
+                arg1: email2.IsValidEmail());
         }
     }
 }
